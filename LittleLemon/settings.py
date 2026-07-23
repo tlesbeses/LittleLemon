7b0dc2b918a4a6980c922d0ef4fcf9b48600baaf
Pevin <pevin.kevin@gmail.com>
2026-07-23 16:37:51 -0600

    Add pagination configuration to REST framework settings

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -138,5 +138,10 @@ REST_FRAMEWORK = {
     "DEFAULT_THROTTLE_RATES": {
         "anon": "10/minute",
         "user": "30/minute",
-    }
+    },
+    
+    "DEFAULT_PAGINATION_CLASS":
+        "rest_framework.pagination.PageNumberPagination",
+
+    "PAGE_SIZE": 10,
 }
