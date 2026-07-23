93001e75ae583168d7a8f5e926cb8da30fc91a08
Pevin <pevin.kevin@gmail.com>
2026-07-23 16:29:12 -0600

    Add django_filters to INSTALLED_APPS and configure REST framework filter backends

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
+    "django_filters",
     ]
 
 MIDDLEWARE = [
@@ -124,4 +125,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
+      "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
+    ],
 }
